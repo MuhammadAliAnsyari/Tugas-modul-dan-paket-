0ddfac2e5493f2782e2d224779274662534415b0
Muhammad Ali Ansyari <49295008+MuhammadAliAnsyari@users.noreply.github.com>
2019-10-15 23:42:32 +0800

    Add files via upload

---
+++ New folder/6.py
@@ -0,0 +1,13 @@
+import balok
+
+vol_balok = balok.vol_balok(5,4,3)
+print('volume balok',vol_balok)
+
+vol_tabung = balok.vol_tabung(5,10)
+print('volume tabung',vol_tabung)
+
+luas_tabung = balok.luas_tabung(5,10)
+print('luas tabung',luas_tabung)
+
+luas_lingkaran = balok.luas_lingkaran(5)
+print('luas lingkaran', luas_lingkaran)
---
+++ New folder/balok.py
@@ -0,0 +1,13 @@
+import math
+
+def vol_balok(p, l, t):
+    return p * l * t
+
+def vol_tabung(t, j):
+    return 3.14 * (j*j) * t
+
+def luas_tabung(t, j):
+    return 2 * 3.14 * (t*j)
+
+def luas_lingkaran(r):
+    return 3.14 * r * r
